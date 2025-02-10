a2c61fbe8a1943f9cbf62b790747520502d0c9ba
Rachel Heaven <reh@bgs.ac.uk>
2025-02-10 20:56:51 +0000

    extending the SQL for building croissant file to include fileset and extract

--- utilities/oracle-to-recordset-fields-json.sql
+++ utilities/oracle-to-recordset-fields-json.sql
@@ -4,17 +4,32 @@
 --    of Croissant JSON-LD
 -- Not all Oracle data types have been mapped to schema.org data types, only those commonly used in BGS
 -- Rachel Heaven, BGS 2025-02-03   
+with table_data as (select 
+      'QL_ACD_ITEM_PT' as table_name,
+      'acditem' as table_abbr,
+      to_char(sysdate,'YYYYMMDD') as version_char,
+      'csv-items' as data_source_key
+      from dual) 
 select json_arrayagg (
     json_object (
         key '@type' value 'cr:Field',
-        key '@id' value 'mytable/'||col_name,
-        key 'name' value 'mytable/'||col_name,
+        key '@id' value td.table_abbr||'/'||col_name,
+        key 'name' value td.table_abbr||'/'||col_name,
         key 'description' value col_comments,
-        key 'dataType' value col_data_type_sc
+        key 'dataType' value col_data_type_sc,
+        key 'source' value json_object (
+              key 'fileSet' value json_object (
+                                  key '@id' value 'csv-items' 
+                                  ),
+              key 'extract' value json_object (
+                                  key 'column' value col_name
+                                  )
+              )
     ) format json returning clob 
 ) as json_doc
-from 
+from table_data td join
 (select 
+tc.table_name,
 lower(tc.column_name) as col_name, 
 cc.comments as col_comments, 
 tc.data_type,
@@ -38,4 +53,6 @@ case tc.DATA_TYPE
         cc.owner=tc.owner 
         and cc.TABLE_NAME=tc.TABLE_NAME 
         and cc.COLUMN_NAME=tc.COLUMN_NAME
-  where tc.owner='MYSCHEMA' and tc.table_name = 'MYTABLE');
+        where tc.owner='BGS'
+   ) c 
+  on td.table_name=c.table_name
