82b35145bea6ec3137002e6d6de1b97e591ea28f
Itahisa Gonzalez Alvarez <itaga@bgs.ac.uk>
2025-03-31 09:50:43 +0100

    Added function to get image urls from new json file.

--- ai-ml-demos/bgs/croissant_functions.py
+++ ai-ml-demos/bgs/croissant_functions.py
@@ -1,7 +1,9 @@
-import mlcroissant as mlc
-import glob
 import os
+import json
+import glob
 import requests
+import mlcroissant as mlc
+from jsonpath_ng import jsonpath, parse
     
 def clear_cache (directory):
         
@@ -93,4 +95,52 @@ def get_images_urls (ds, record_set):
     
     
     
+
+def get_images_urls_api (ds, record_set):
+    
+    # Get field names:
+    fields = record_set[0].fields
+    records: mlc.Records = ds.records(record_set=record_set[0].id)  # columns
+
+    # Create empty lists for image urls and names:
+    ppl_urls = []; xpl_urls = []; sample_names = []
+
+    for r, record in enumerate(records):
+        for f, field in enumerate(record_set[0].fields):
+            
+            if 'json-image-urls-field-id' in field.id:
+                if record.get(field.id)!= None:                             # One of the records doesn't have any image urls?
+                    
+                    # Parse the JSON string into a Python object
+                    json_string = record.get(field.id).decode('utf-8').replace("'", "\"")
+                    dict_list = json.loads(json_string)
+                    # print (dict_list)
+                    
+                    jsonpath_expression = parse('$[*]') # Let's look at all the dictionaries in the list
+                    
+                    matches = [match.value for match in jsonpath_expression.find(dict_list)]
+
+                    # Get links for all PPL and XPL image pairs with link_type='web' and JPG format:
+                    ppl_link = [item['image_link'] for item in matches
+                                      if item['light_type'] == "PPL" and item['link_type'] == "web" and item['image_format'] == "JPG"][0]
+                    ppl_urls.append ('https:' + ppl_link)
+                    # print(xpl_link)
+                    
+                    xpl_link = [item['image_link'] for item in matches
+                                      if item['light_type'] == "XPL" and item['link_type'] == "web" and item['image_format'] == "JPG"][0]
+                    xpl_urls.append ('https:' + xpl_link)
+                    
+                # else:
+                    # print (r, record, f, field)
+                    # print (record.get('json-image-urls-field-id'))
+
+            if 'sample-name' in field.id and record.get('json-image-urls-field-id')!=None:
+                # print(record.get(field.id).decode('utf-8'))
+                sample_names.append (record.get(field.id).decode('utf-8'))
+
+            
+    return ppl_urls, xpl_urls, sample_names
+            
+            
+
     
